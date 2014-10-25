1e7144c43ac4c29c78b40d90d1949478d0508480
oldfeel <hyt5926@163.com>
2014-10-26 03:22:16 +0800

    添加活动模块儿.

---
+++ src/com/yuncommunity/theme/ios/adapter/IActivityAdapter.java
@@ -0,0 +1,26 @@
+package com.yuncommunity.theme.ios.adapter;
+
+import android.content.Context;
+import android.view.View;
+
+import com.oldfeel.base.BaseBaseAdapter;
+import com.yuncommunity.item.InformationItem;
+
+/**
+ * 
+ * @author oldfeel
+ * 
+ *         Create on: 2014年10月26日
+ */
+public class IActivityAdapter extends BaseBaseAdapter<InformationItem> {
+
+	public IActivityAdapter(Context context) {
+		super(context);
+	}
+
+	@Override
+	public View getView(int position, View view) {
+		return null;
+	}
+
+}
--- src/com/yuncommunity/theme/ios/fragment/IActivityFragment.java
+++ src/com/yuncommunity/theme/ios/fragment/IActivityFragment.java
@@ -1,6 +1,12 @@
 package com.yuncommunity.theme.ios.fragment;
 
-import com.oldfeel.base.BaseFragment;
+import android.content.Intent;
+
+import com.oldfeel.base.BaseListFragment;
+import com.oldfeel.utils.NetUtil;
+import com.yuncommunity.item.InformationItem;
+import com.yuncommunity.theme.ios.IActivityDetail;
+import com.yuncommunity.theme.ios.adapter.IActivityAdapter;
 
 /**
  * 活动
@@ -9,6 +15,22 @@ import com.oldfeel.base.BaseFragment;
  * 
  *         Create on: 2014年10月25日
  */
-public class IActivityFragment extends BaseFragment {
+public class IActivityFragment extends BaseListFragment {
+	public static IActivityFragment newInstance(NetUtil netUtil) {
+		IActivityFragment fragment = new IActivityFragment();
+		fragment.netUtil = netUtil;
+		return fragment;
+	}
 
+	@Override
+	public void onItemClick(int position) {
+		InformationItem item = (InformationItem) adapter.getItem(position);
+		startActivity(new Intent(getActivity(), IActivityDetail.class)
+				.putExtra("item", item));
+	}
+
+	@Override
+	public void initAdapter() {
+		adapter = new IActivityAdapter(getActivity());
+	}
 }
